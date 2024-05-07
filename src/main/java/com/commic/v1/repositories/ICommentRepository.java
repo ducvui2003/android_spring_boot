8486bd54e7e93e0228d43d22b65031b9bc391e41
Lê Anh Đức <ducvui2003@gmail.comc>
2024-05-07 22:20:56 +0700

    feature: comment book

--- src/main/java/com/commic/v1/repositories/ICommentRepository.java
+++ src/main/java/com/commic/v1/repositories/ICommentRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICommentRepository extends JpaRepository<Comment, Integer> {
-
     Page<Comment> findAllByStateOrderByCreatedAtDesc(String value, Pageable pageable);
+
 }

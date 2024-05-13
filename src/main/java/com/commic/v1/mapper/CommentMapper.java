0134e4ab015745f348f6c58f53feccc0fba1c26b
ThanhBinh2219 <21130291@st.hcmuaf.edu.vn>
2024-05-13 14:48:14 +0700

    feat: create function manage comment

--- src/main/java/com/commic/v1/mapper/CommentMapper.java
+++ src/main/java/com/commic/v1/mapper/CommentMapper.java
@@ -5,12 +5,17 @@ import com.commic.v1.dto.requests.CommentCreationRequestDTO;
 import com.commic.v1.dto.responses.CommentCreationResponseDTO;
 import com.commic.v1.entities.Comment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
+
     Comment toComment(CommentCreationRequestDTO comment);
     CommentCreationResponseDTO toCommentCreationRequestDTO(Comment comment);
-    List<CommentDTO> toCommentDTOs(List<Comment> comments);
+
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "chapter.id", target = "chapterId")
+    CommentDTO toCommentDTOs(Comment comment);
 }
